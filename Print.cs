c2b83413f93f38fc1b10ebbcbe8652ded86c29bf
ilya klevesenkov <ilya.klevesenkov@gmail.com>
2020-03-29 14:39:53 +0300

    тестирование

--- Print.cs
+++ Print.cs
@@ -11,13 +11,12 @@ namespace PrintFromFolder
         public void PrintDOC(string path, string name, System.Windows.Forms.DataGridView dg)
         {
             Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application { Visible = false };
-            Microsoft.Office.Interop.Word.Document doc = new Microsoft.Office.Interop.Word.Document();
-        //word.Visible = false;
+            Microsoft.Office.Interop.Word.Document doc = new Microsoft.Office.Interop.Word.Document();        
             doc = word.Documents.Open(path+"\\"+name);
             doc.PrintOut();
             dg.Rows.Add(name, DateTime.Now.ToString());
             doc.Close();
-            word.ActiveWindow.Close();
+            word.Application.Quit();
         }
     }
 }
